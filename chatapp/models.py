d91d66110976fde51571ea9d9847c808f570506c
박관우 <dirn0568@naver.com>
2021-09-03 23:27:58 +0900

    chat_system

--- chatapp/models.py
+++ chatapp/models.py
@@ -7,10 +7,10 @@ class ChatModel(models.Model):
     send_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='send_user')
     receive_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receive_user')
 
-    chat_line = models.CharField(max_length=300)
-    chat_img = models.ImageField(upload_to='chat_test_img/', null=True, blank=True)
+    chat_line = models.CharField(max_length=300, null=True, blank=True)
+    chat_img = models.FileField(upload_to='chat_file/', null=True, blank=True)
 
-    chat_time = models.DateField(auto_now_add=True)
+    chat_time = models.DateTimeField(auto_now=True)
 
 # class test(models.Model)
 
