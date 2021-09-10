baf80adde76f7fbe76763116dcbee82daff7aa26
박관우 <dirn0568@naver.com>
2021-09-10 23:55:51 +0900

    chat_system

--- chatapp/forms.py
+++ chatapp/forms.py
@@ -5,7 +5,7 @@ from chatapp.models import ChatModel
 
 
 class ChatForm(forms.ModelForm):
-    chat_line = forms.CharField(widget=forms.Textarea(attrs={"rows":5, "cols":20}))
+    chat_line = forms.CharField(widget=forms.Textarea(attrs={"cols":20, "rows":5, "min_length":0}))
 
     class Meta:
         model = ChatModel
