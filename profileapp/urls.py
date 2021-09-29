dce96a66d6545b7caf9b870b91ddf725b6412a51
박관우 <dirn0568@naver.com>
2021-09-29 23:53:21 +0900

    profile_system

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from accountapp import views
 from accountapp.views import Create_User, Detail_User, Update_User, Delete_User
+from profileapp import views
 from profileapp.views import Create_Profile_View
 
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('create_profile/<int:pk>/', Create_Profile_View.as_view(), name='create_profile'),
+    path('create_profile/<int:pk>/', views.create_profile, name='create_profile'),
     path('detail_profile/<int:pk>/', Detail_User.as_view(), name='detail_profile'),
     path('update_profile/<int:pk>/', Update_User.as_view(), name='update_profile'),
     path('delete_profile/<int:pk>/', Delete_User.as_view(), name='delete_profile'),
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,21 +1,30 @@
-from django.shortcuts import render
+import self
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 
+from accountapp.models import User
 from profileapp.forms import ProfileForm, Update_ProfileForm
 from profileapp.models import User_Profile
 
 
 class Create_Profile_View(CreateView):
     model = User_Profile
+    pk_url_kwarg = 'target_pk'
+    context_object_name = 'target_profile'
     form_class = ProfileForm
     template_name = 'create_profile.html'
 
-    def form_valid(self, form):
+    def get_object(self, queryset=None):
+        print('작동되고있긴한가1351353151513531531')
+        return super().form_valid(self.pk)
+
+    def form_valid(self, form, pk):
+        print("실앻ㅇ주우우우웅")
         temp_profile = form.save(commit=False)
-        temp_profile.profile = self.request.user
+        temp_profile.pk = self.request.user
         temp_profile.save()
         return super().form_valid(form)
 
@@ -36,3 +45,19 @@ class Delete_Profile_View(DeleteView):
     context_object_name = 'target_profile'
     template_name = ''
     success_url = reverse_lazy('mainapp:main')
+
+def create_profile(request, pk):
+    if request.method == "POST":
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            temp_form = form.save(commit=False)
+            temp_instance =  User.objects.filter(pk=pk)
+            for temp in temp_instance:
+                temp_form.profile = temp
+            temp_form.save()
+            return redirect('accountapp:detail_user', pk)
+    context = {}
+    context['profile_form'] = ProfileForm
+    context['pk'] = pk
+    return render(request, 'create_profile.html', context)
+
